b86cf99d378e22ac1103dde52f3382770ac11284
kuromi9527 <145821716+kuromi9527@users.noreply.github.com>
2023-12-18 14:27:30 +0800

    Update LSTM.py

--- syntradic/factors/LSTM.py
+++ syntradic/factors/LSTM.py
@@ -1,1 +1,42 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+# 时间序列预测的 LSTM 模型示例
+
+class LSTMTimeSeriesModel:
+    def __init__(self, input_shape, units=50):
+        self.model = Sequential()
+        self.model.add(LSTM(units, input_shape=input_shape))
+        self.model.add(Dense(1))
+        self.model.compile(optimizer='adam', loss='mean_squared_error')
+
+    def train(self, X_train, y_train, epochs=10, batch_size=32):
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+# 示例用法
+
+# 假设 data 是包含时间序列数据的 DataFrame
+# 您需要预处理数据以适应 LSTM 模型。
+# 这通常包括重新调整数据形状和标准化。
+
+# 为了演示，我们创建一些虚拟数据
+data = pd.DataFrame(np.random.rand(1000, 1), columns=['value'])
+
+# 在这里预处理您的数据（例如，标准化、重新调整形状）
+
+# X_train, y_train = ... （您的训练数据）
+# X_test = ... （您的测试数据）
+
+# 初始化并训练模型
+# input_shape 应该与您的训练数据形状相匹配
+# model = LSTMTimeSeriesModel(input_shape=(...))
+# model.train(X_train, y_train)
+
+# 进行预测
+# predictions = model.predict(X_test)
 
